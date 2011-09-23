ddf08a748c69615d49964c86dbb6e21b2a7087d4
vd <v.dorrich@gmail.com>
2011-09-23 23:30:22 +0200

    ccodegen

--- ccodegen.py
+++ ccodegen.py
@@ -11,6 +11,29 @@ from implement import *
 
 # ------------------------------------------------------------------------------------------------------------
 
+__operators = {
+	"xor" :		lambda n, args : "(" + string.join(args, "^") + ")",
+	"or" :		lambda n, args : "(" + string.join(args, "|") + ")",
+	"nor" :		lambda n, args : "!(" + string.join(args, "|") + ")",
+	"and" :		lambda n, args : "(" + string.join(args, "&") + ")",
+	"nand" :	lambda n, args : "!(" + string.join(args, "&") + ")",
+	"not" :		lambda n, (arg,) : "!(" + arg + ")",
+	"add" :		lambda n, args : "(" + string.join(args, "+") + ")",
+	"sub" :		lambda n, args : "(" + string.join(args, "-") + ")",
+	"mul" :		lambda n, args : "(" + string.join(args, "*") + ")",
+	"div" :		lambda n, args : "(" + string.join(args, "/") + ")",
+	"mod" :		lambda n, args : "(" + string.join(args, "%") + ")",
+	#"divmod"
+}
+
+def __implement(n, args, outs) :
+	if n.prototype.type_name in __operators :
+		assert(len(args) >= 2)
+		assert(len(outs) == 1)
+		return __operators[n.prototype.type_name](n, args)
+	else :
+		return n.prototype.exe_name + "(" + string.join(args + outs, ", ") + ")"
+
 # execution
 def __post_visit(g, code, tmp, subtrees, expd_dels, n, visited) :
 
@@ -20,19 +43,26 @@ def __post_visit(g, code, tmp, subtrees, expd_dels, n, visited) :
 		return None # handled elsewhere
 
 	inputs, outputs = g[n]
-#	assert(outputs == g[n].s)
-#	assert(inputs == g[n].p)
 
 	args = []
 	outs = []#TODO TODO TODO
 
-#	print n
+	print "__post_visit:", n, tmp, subtrees
+
+	for out_term, out_t_nr, succs in outputs :
+		print "out_term, out_t_nr, succs =", n, out_term, out_t_nr, succs
+		if len(succs) > 1 or (len(outputs) > 1 and len(succs) == 1):
+			print "adding temps:", succs
+			slot = add_tmp_ref(tmp, succs)
+			outs.append("&tmp%i"%slot)
+		else :
+			outs.append("&dummy")
 
 	#gather inputs
 	for in_term, in_t_nr, preds in inputs :
 		assert(len(preds)==1)
 		((m, m_t, m_t_nr), ) = preds
-#		print "\tgathering:", m, m_t, m_t_nr, "for", (n, in_term, in_t_nr), "subtrees:", subtrees
+		print "\tgathering:", m, m_t, m_t_nr, "for", (n, in_term, in_t_nr), "subtrees:", subtrees, "tmp:", tmp
 		if isinstance(m.prototype, core.ConstProto) :
 			assert(m.value != None)
 			args.append(str(m.value))
@@ -43,18 +73,8 @@ def __post_visit(g, code, tmp, subtrees, expd_dels, n, visited) :
 			if slot != None:
 				args.append("tmp%i" % slot)
 			else :
-#				print tmp
-				raise Exception("holy shit!")
-
-	if len(outputs) > 0 :
-		for out_term, out_t_nr, succs in outputs :
-			#TODO outs!!!
-#			print "out_term, out_t_nr, succs =", out_term, out_t_nr, succs
-			if len(succs) > 0 :
-				slot = add_tmp_ref(tmp, succs)
-				outs.append("&tmp%i"%slot)
-			else :
-				outs.append("&dummy")
+				raise Exception("holy shit! %s not found, %s %s" %
+					(str((n, in_term, in_t_nr)), str(tmp), str(subtrees)))
 
 	if isinstance(n.prototype, core.DelayInProto) :
 		del_in, del_out = expd_dels[n.delay]
@@ -77,12 +97,13 @@ def __post_visit(g, code, tmp, subtrees, expd_dels, n, visited) :
 	else :
 		assert(n.prototype.exe_name != None)
 #		exe_name = n.prototype.exe_name
-		expr = n.prototype.exe_name + "(" + string.join(args + outs, ", ") + ")"
+		expr = __implement(n, args, outs)
+#		expr = n.prototype.exe_name + "(" + string.join(args + outs, ", ") + ")"
 
 #	expr = exe_name + "(" + string.join(args, ", ") + string.join(outs, ", ") + ")"
 	is_expr = len(outputs) == 1 and len(outputs[0][2]) == 1
 
-#	print "\texpr:", expr, "is_expr:", is_expr
+	print "\texpr:", expr, "is_expr:", is_expr, "tmp=", tmp
 
 	if is_expr :
 		((out_term, out_t_nr, succs), ) = outputs
@@ -101,11 +122,15 @@ def __post_visit(g, code, tmp, subtrees, expd_dels, n, visited) :
 
 def codegen_alt(g, expd_dels, meta) :
 
+
+	pprint(g)
+
 	tmp = temp_init()
 	subtrees = {}
 	code = []
 	dft_alt(g, post_visit = partial(__post_visit, g, code, tmp, subtrees, expd_dels))
 
+	print tmp
 	assert(tmp_used_slots(tmp) == 0)
 	assert(len(subtrees) == 0)
 
