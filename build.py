255b3cf898e18f5889ae22d4e4517b5825e57782
vd <v.dorrich@gmail.com>
2011-10-12 21:23:36 +0200

    builder script

--- build.py
+++ build.py
@@ -1,35 +1,88 @@
 
 from sys import exit
+import subprocess
+import os
+import tempfile
 
 # ----------------------------------------------------------------------------
 
-class Job(object) :
-	pass
+#class __sink(object) :
+#	def write(self, s) :
+#		self.__data += s
+#	def __init__(self) :
+#		self.__data = ""
 
-# ----------------------------------------------------------------------------
+#		p = subprocess.Popen(["avr-size", a_out], bufsize=0, executable=None, stdin=None, stdout=subprocess.PIPE,
+#			stderr=subprocess.PIPE, preexec_fn=None, close_fds=False, shell=False,
+#		cwd=os.getcwd(), env=None, universal_newlines=False, startupinfo=None, creationflags=0)
 
-class ImplementerWrap(object) :
-	pass
 
-# ----------------------------------------------------------------------------
+def build() :
+	workdir = os.getcwd()
 
-class AVRGCCWrap(object) :
-	pass
+#	a_out_f = tempfile.NamedTemporaryFile()
+#	a_hex_f = tempfile.NamedTemporaryFile()
+#	a_out = a_out_f.name
+#	a_hex = a_hex_f.name
 
-# ----------------------------------------------------------------------------
+	a_out, a_hex = "a.out", "a.hex"
 
-class AvrdudeWrap(object) :
-	pass
+	optimization = "-Os"
+	mcu = "atmega328"
+	f_cpu = 16000000
 
-# ----------------------------------------------------------------------------
+	gcc_args = [ optimization, "-mmcu=" + mcu, "-DF_CPU=%i" % f_cpu, "-o", a_out ]
 
-class ArduinoJob(Job) :
-	pass
+	try :
+		p = subprocess.Popen(["avr-gcc"] + gcc_args,
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE,
+			cwd=workdir)
+	except :
+		print("failed to execute avr-gcc")
+	else :
+		(stdoutdata, stderrdata) = p.communicate()
+		if p.returncode == 0 :
+			print("compiled")
+		else :
+			print("failed to execute avr-gcc")
+
+	try :
+		p = subprocess.Popen(["avr-size", a_out],
+			stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE,
+			cwd=workdir)
+	except :
+		print("failed to execute avr-size")
+	else :
+		(stdoutdata, stderrdata) = p.communicate()
+		if p.returncode == 0 :
+			head, val = stdoutdata.split(os.linesep)[0:2]
+			sizes = dict(zip(head.split(), val.split()))
+			print("memory usage: flash %iB, ram %iB" %
+				(int(sizes["text"]), int(sizes["data"])+int(sizes["bss"])))
+		else :
+			print("failed to execute avr-size")
+
+	try :
+		p = subprocess.Popen(
+			["avr-objcopy", "-j", ".text", "-j", ".data" "-O" "ihex", a_out, a_hex],
+			cwd=workdir)
+	except :
+		print("failed to execute avr-objcopy")
+	else :
+		p.communicate()
+		if p.returncode == 0 :
+			pass
+		else :
+			print("failed to execute avr-objcopy")
+
+	exit(666)
 
 # ----------------------------------------------------------------------------
 
 if __name__ == "__main__" :
-	exit(666)
+	build()
 
 # ----------------------------------------------------------------------------
 
---
+++ dummy.c
@@ -0,0 +1,3 @@
+static char x [10];
+char xy [10];
+int main(){int x; while(1); return 666; }
