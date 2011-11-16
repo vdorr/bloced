81e9c3b2b5442fec5cbfc7c2f3691e348154a80d
vd <v.dorrich@gmail.com>
2011-11-16 22:48:28 +0100

    build.py

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 
 ####import sys
-from sys import exit
+import sys
 import subprocess
 import os
 import tempfile
@@ -13,7 +13,6 @@ from pprint import pprint
 # ----------------------------------------------------------------------------
 
 #TODO use as gedit plugin
-#TODO ".buildignore"
 
 def __run_external(args, workdir=None) :
 	try :
@@ -31,84 +30,102 @@ def __run_external(args, workdir=None) :
 			return (False, p.returncode, (stdoutdata, stderrdata))
 
 
+#def __list_files(workdir, recurse) :
+#	file_list = []
+#	tree = os.walk(workdir)
+#	for root, dirs, files in tree if recurse else [ tree.next() ] :
+#		file_list += [ os.path.join(workdir, root, fn) for fn in files ]
+#	return file_list
+
+
+__src_exts = ( "*.c", "*.cpp" )
+__hdr_exts = ( "*.h", "*.hpp" )
+
+__re_src = re.compile("("+string.join([ fnmatch.translate(ln) for ln in __src_exts ], ")|(")+")")
+__re_hdr = re.compile("("+string.join([ fnmatch.translate(ln) for ln in __hdr_exts ], ")|(")+")")
+
+
 def __list_files(workdir, recurse) :
-	file_list = []
+	sources = []
+	inlude_dirs = []
 	tree = os.walk(workdir)
 	for root, dirs, files in tree if recurse else [ tree.next() ] :
-		file_list += [ os.path.join(workdir, root, fn) for fn in files]
-	return file_list
+		sources += [ os.path.join(workdir, root, fn)
+			for fn in files if __re_src.match(fn) ]
+		if any([ __re_hdr.match(fn) for fn in files ]) :
+			inlude_dirs.append(os.path.join(workdir, root))
+	return sources, inlude_dirs
+
+
+def __read_lines(file_name) :
+	try :
+		f = open(file_name, "r")
+		lines = f.readlines()
+		f.close()
+		return lines
+	except :
+		return None
 
 
 def __read_ignore(fname) :
-	try :
-		f = open(fname, "r")
-		lines = f.readlines()
-		f.close()
+	lines = __read_lines(fname)
+	if lines != None :
 		return [ ln.strip() for ln in lines if ln.strip() ]
-	except :
+
+
+def __parse_boards(boards_txt) :
+	lines = __read_lines(boards_txt)
+	if lines == None :
 		return None
-#fnmatch.filter(names, pattern)
-
-
-def __parse_boards_line(line) :
-	key, value = line.split("=")
-	return line
-
-
-def __parse_boards(board_txt) :
-	try :
-		f = open(board_txt, "r")
-		lines = f.readlines()
-		f.close()
-	except :
-		return None
-
 	valid = re.compile("\s*\w+\.\S+\s*=\s*\S+")
-
-#	items = [ __parse_boards_line(ln) for ln in lines if valid.match(ln) ]
-
 	board_info = {}
 	for line in [ ln for ln in lines if valid.match(ln) ] :
 		key, value = line.split("=")
 		dot = key.index(".")
-		board_name = key[0:dot]
+		board_name = key[0:dot].strip()
 		if not board_name in board_info :
 			board_info[board_name] = {}
-		board_info[board_name][key[dot+1:]] = value.strip()
-
+		board_info[board_name][key[dot+1:].strip()] = value.strip()
 	return board_info
 
 
-__source_file_exts = [
-	"*.c",
-	"*.h",
-	"*.cpp",
-	"*.hpp",
-]
-
 def build() :
+	boards_txt = "/usr/share/arduino/hardware/arduino/boards.txt"
+	arduino_src_dir = "/usr/share/arduino/hardware/arduino/cores/arduino"
+	board = "uno"
+	ignore_file = ".amkignore"
+	prog_port = "/dev/ttyACM0"
 	workdir = os.getcwd()
+	wdir_recurse = True
 
-	ignores = __read_ignore(os.path.join(workdir, ".amkignore"))
+	ignores = __read_ignore(os.path.join(workdir, ignore_file))
 	ign_res = [ fnmatch.translate(ln) for ln in ignores ]
 #	print "("+string.join(ign_res, ")|(")+")"
 	re_ignore = re.compile("("+string.join(ign_res, ")|(")+")")
 
-	src_res = [ fnmatch.translate(ln) for ln in __source_file_exts ]
-	re_source = re.compile("("+string.join(src_res, ")|(")+")")
+#	src_res = [ fnmatch.translate(ln) for ln in __src_exts ]
+#	re_source = re.compile("("+string.join(src_res, ")|(")+")")
 #	print "("+string.join(src_res, ")|(")+")"
 
-	files = __list_files(workdir, True)
+#	files = __list_files(workdir, True)
 #	print files
-	sources = [ fn for fn in files
-		if re_source.match(fn) and not re_ignore.match(fn) ]
-	print sources
+#	sources = [ fn for fn in files
+#		if re_source.match(fn) and not re_ignore.match(fn) ]
 
-	board_txt = "/usr/share/arduino/hardware/arduino/boards.txt"
-	board_info = __parse_boards(board_txt)
-	pprint(board_info)
+	ard_sources, ard_idirs = [], []#__list_files(arduino_src_dir, False)#TODO
 
-	exit(0)
+	sources, loc_idirs = __list_files(workdir, wdir_recurse)
+
+	board_info = __parse_boards(boards_txt)
+#	pprint(board_info)
+
+
+	print board_info[board]["name"]
+
+	mcu = board_info[board]["build.mcu"]
+	f_cpu = board_info[board]["build.f_cpu"]
+	flash_size = int(board_info[board]["upload.maximum_size"])
+	prog_mcu = mcu.capitalize() #"m328p"
 
 	run = partial(__run_external, workdir=workdir)
 
@@ -117,46 +134,34 @@ def build() :
 #	a_out = a_out_f.name
 #	a_hex = a_hex_f.name
 
+	board_idirs = [ "/usr/lib/avr", "/usr/lib/avr/include",
+		"/usr/lib/avr/util", "/usr/lib/avr/compat" ]
 	a_out, a_hex = "a.out", "a.hex"
-	i_dirs = []#[ "/usr/lib/avr", "/usr/lib/avr/include", "/usr/lib/avr/util", "/usr/lib/avr/compat" ]
-#"/usr/share/arduino/hardware/arduino/cores/arduino"
+	i_dirs = [ "-I" + d for d in ( loc_idirs + board_idirs + ard_idirs ) ]
+#""
 	l_libs = []#[ "/usr/lib/avr/lib/libc.a" ]
 
 	optimization = "-Os"
-	mcu = "atmega328"
-	f_cpu = 16000000
-	
-	prog_mcu = "m328p"
-	prog_port = "/dev/ttyACM0"
 
-	gcc_args = i_dirs + l_libs + [ optimization, "-mmcu=" + mcu, "-DF_CPU=%i" % f_cpu, "-o", a_out ]
-	gcc_args += ["tst.c"]
+	gcc_args = i_dirs + l_libs + [ optimization, "-mmcu=" + mcu,
+		"-DF_CPU=%s" % f_cpu, "-o", a_out ]
+	gcc_args += sources + ard_sources#["tst.c"]
 
-	success, _, __ = run(["avr-gcc"] + gcc_args)
+	success, _, streams = run(["avr-gcc"] + gcc_args)
 	if success :
-		print("compiled")
+		stdoutdata, stderrdata = streams
+		print("compiled" + " " + stdoutdata + stderrdata)
 	else :
-		print("failed to execute avr-gcc")
+		stdoutdata, stderrdata = streams
+		print("failed to execute avr-gcc" + " " + stdoutdata + stderrdata)
 		sys.exit(10)
-#	try :
-#		p = subprocess.Popen(["avr-gcc"] + gcc_args,
-#			stdout=subprocess.PIPE,
-#			stderr=subprocess.PIPE,
-#			cwd=workdir)
-#	except :
-#		print("failed to execute avr-gcc")
-#	else :
-#		(stdoutdata, stderrdata) = p.communicate()
-#		if p.returncode == 0 :
-#			print("compiled")
-#		else :
-#			print("failed to execute avr-gcc")
 
 	success, rc, streams = run(["avr-size", a_out])
 	if success :
 		stdoutdata, _ = streams
 		head, val = stdoutdata.split(os.linesep)[0:2]
 		sizes = dict(zip(head.split(), val.split()))
+#TODO check against boards.txt
 		print("memory usage: flash %iB, ram %iB" %
 			(int(sizes["text"]),
 			int(sizes["data"])+int(sizes["bss"])))
@@ -164,68 +169,33 @@ def build() :
 		print("failed to execute avr-size")
 		sys.exit(20)
 
-
-#	try :
-#		p = subprocess.Popen(["avr-size", a_out],
-#			stdout=subprocess.PIPE,
-#			stderr=subprocess.PIPE,
-#			cwd=workdir)
-#	except :
-#		print("failed to execute avr-size")
-#	else :
-#		(stdoutdata, stderrdata) = p.communicate()
-#		if p.returncode == 0 :
-#			head, val = stdoutdata.split(os.linesep)[0:2]
-#			sizes = dict(zip(head.split(), val.split()))
-#			print("memory usage: flash %iB, ram %iB" %
-#				(int(sizes["text"]), int(sizes["data"])+int(sizes["bss"])))
-#		else :
-#			print("failed to execute avr-size")
-
-	success, _, __ = run(["avr-objcopy", "-j", ".text", "-j", ".data" "-O" "ihex", a_out, a_hex])
+	success, _, __ = run(["avr-objcopy", "-j", ".text", "-j",
+		".data" "-O" "ihex", a_out, a_hex])
 	if success :
 		print("hex file created")
 	else :
 		print("failed to execute avr-objcopy")
 		sys.exit(30)
 
-#	try :
-#		p = subprocess.Popen(
-#			["avr-objcopy", "-j", ".text", "-j", ".data" "-O" "ihex", a_out, a_hex],
-#			cwd=workdir)
-#	except :
-#		print("failed to execute avr-objcopy")
-#	else :
-#		p.communicate()
-#		if p.returncode == 0 :
-#			pass
-#		else :
-#			print("failed to execute avr-objcopy")
 
-	success, _, streams = run(["avrdude", "-q", "-n", "-c"+"arduino", "-P" + prog_port, "-p" + prog_mcu, "-Uflash:w:" + a_hex + ":i"])
-#	success, _, streams = run(["avrdude", "-q", "-n", "-c"+"arduino", "-Pusb", "-p" + prog_mcu, "-U flash:w:" + a_hex + ":i"])
+
+
+	sys.exit(0)
+
+
+
+
+
+	success, _, streams = run(["avrdude", "-q", "-n", "-c"+"arduino",
+		"-P" + prog_port,
+		"-p" + prog_mcu,
+		"-Uflash:w:" + a_hex + ":i"])
 	if success :
 		print("succesfully uploaded")
 	else :
 		stdoutdata, stderrdata = streams
 		print("failed to run avrdude '%s'" % stderrdata)
 		sys.exit(40)
-
-#	try :
-#		p = subprocess.Popen(
-#			["avrdude", "-q", "-n", "-c"+"arduino", "-P " + prog_port, "-p" + prog_mcu, "-U flash:w:" + a_hex + ":i"],
-#			cwd=workdir)
-#	except :
-#		print("failed to run avrdude")
-#	else :
-#		p.communicate()
-#		if p.returncode == 0 :
-#			pass
-#		else :
-#			print("failed to run avrdude")
-
-#XXX
-#	exit(666)
 
 # ----------------------------------------------------------------------------
 
