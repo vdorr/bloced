f4fda9ef0edc8d92da02487f27528c0c8ddd9fa1
vd <v.dorrich@gmail.com>
2011-12-30 20:10:06 +0100

    modifications for rh

--- mybuild.py
+++ mybuild.py
@@ -9,28 +9,51 @@ if __name__ == "__main__" :
 		"uno_dfu" : {
 			"name" : "uno board dfu",
 			"build.mcu" : "atmega8u2",
-			"build.f_cpu" : "16000000L", "upload.maximum_size" : "7168"
+			"build.f_cpu" : "16000000L",
+			"upload.maximum_size" : "7168"
 		},
 		"8u2@16M" : {
 			"name" : "8u2@16M",
 			"build.mcu" : "atmega8u2",
-			"build.f_cpu" : "16000000L", "upload.maximum_size" : "7168"
+			"build.f_cpu" : "16000000L",
+			"upload.maximum_size" : "7168"
+		},
+		"8u2@8M" : {
+			"name" : "8u2@8M",
+			"build.mcu" : "atmega8u2",
+			"build.f_cpu" : "8000000L",
+			"upload.maximum_size" : "7168"
 		}
 	}
 #	AUX_SRC_DIR = "/usr/share/arduino/hardware/arduino/cores/arduino"
-	rc, = build("uno_dfu", os.getcwd(),
-		wdir_recurse = True,
-		aux_src_dirs = [],#[ src_dir_t(AUX_SRC_DIR, False) ],
-		boards_txt = None, #BOARDS_TXT,
-		ignore_file = "amkignore",
-		prog_port = None,#"/dev/ttyACM0",
-		prog_driver = "dfu-programmer", # or "avrdude"
-		prog_adapter = None, #"arduino", #None for dfu-programmer
-		optimization = "-O0"#default "-Os",
-		verbose = False,
-		skip_programming = False,
-		dry_run = True,
-		board_db=boards)
+	if True :
+		rc, = build("8u2@8M", os.getcwd(),
+			wdir_recurse = True,
+			aux_src_dirs = [],#[ src_dir_t(AUX_SRC_DIR, False) ],
+			boards_txt = None, #BOARDS_TXT,
+			ignore_file = "amkignore",
+			prog_port = None,#"/dev/ttyACM0",
+			prog_driver = "dfu-programmer", # or "avrdude"
+			prog_adapter = None, #"arduino", #None for dfu-programmer
+			optimization = "-O0"#default "-Os",
+			verbose = False,
+			skip_programming = False,
+			dry_run = True,
+			board_db=boards)
+	else :
+		rc, = build("uno_dfu", os.getcwd(),
+			wdir_recurse = True,
+			aux_src_dirs = [],#[ src_dir_t(AUX_SRC_DIR, False) ],
+			boards_txt = None, #BOARDS_TXT,
+			ignore_file = "amkignore",
+			prog_port = None,#"/dev/ttyACM0",
+			prog_driver = "dfu-programmer", # or "avrdude"
+			prog_adapter = None, #"arduino", #None for dfu-programmer
+			optimization = "-O0"#default "-Os",
+			verbose = False,
+			skip_programming = False,
+			dry_run = True,
+			board_db=boards)
 	sys.exit(rc)
 
 
