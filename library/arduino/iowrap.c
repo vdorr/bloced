8b44137629ddb6b3ea3161f6b2ff82c71b99449c
vd <v.dorrich@gmail.com>
2012-02-17 14:18:27 +0100

    reworked arduino library

--- library/arduino/iowrap.c
+++ library/arduino/iowrap.c
@@ -1,65 +1,6 @@
 
-/* ------------------------------------------------------------------------ */
-
-#define _VM_EXPORT_
-
-typedef int vm_word_t;
-typedef long vm_dword_t;
-
 #include "wiring.h"
-
-_VM_EXPORT_ vm_word_t d_in(vm_word_t nr);
-_VM_EXPORT_ void d_out(vm_word_t nr, vm_word_t v);
-_VM_EXPORT_ vm_word_t a_in(vm_word_t nr, vm_word_t a_ref);
-_VM_EXPORT_ void a_out(vm_word_t nr/*, vm_word_t f*/, vm_word_t dc);
-
-_VM_EXPORT_ vm_dword_t time_ms(void);
-_VM_EXPORT_ vm_dword_t time_us(void);
-
-/* _VM_EXPORT_ const vm_word_t = INTERNAL; */
-
-/* ------------------------------------------------------------------------ */
-
-/*#define IF_LAZY_INIT	if*/
-
-#define IF_LAZY_INIT(x)	if (1)
-
-#define DIN_MAP(nr)	(nr)
-#define DOUT_MAP(nr)	(nr)
-#define AIN_MAP(nr)	(nr)
-#define AOUT_MAP(nr)	(nr)
-
-
-
-#define PINMODE_UNINITIALIZED	0
-#define PINMODE_DIGITAL_IN	1
-#define PINMODE_DIGITAL_OUT	2
-#define PINMODE_ANALOG_IN	3
-#define PINMODE_ANALOG_OUT	4
-
-static unsigned char pin_mode[20] =
-{
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-	PINMODE_UNINITIALIZED,
-};
+#include "iowrap.h"
 
 /*
 void init(void);
@@ -80,64 +21,45 @@ unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout);
 void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
 uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
 */
-/* ------------------------------------------------------------------------ */
 
 vm_word_t d_in(vm_word_t nr)
 {
-	IF_LAZY_INIT ( pin_mode[DIN_MAP(nr)] != PINMODE_DIGITAL_IN )
-	{
-		pinMode(nr, INPUT);
-		pin_mode[DIN_MAP(nr)] = PINMODE_DIGITAL_IN;
-		digitalWrite(nr, 1);
-	}
 	return digitalRead(nr);
 }
 
-/* ------------------------------------------------------------------------ */
-
 void d_out(vm_word_t nr, vm_word_t v)
 {
-	IF_LAZY_INIT ( pin_mode[DOUT_MAP(nr)] != PINMODE_DIGITAL_OUT )
-	{
-		pinMode(nr, OUTPUT);
-		pin_mode[DOUT_MAP(nr)] = PINMODE_DIGITAL_OUT;
-	}
 	digitalWrite(nr, v);
 }
 
-/* ------------------------------------------------------------------------ */
-
-/*
-    DEFAULT: the default analog reference of 5 volts (on 5V Arduino boards) or 3.3 volts (on 3.3V Arduino boards)
-    INTERNAL: an built-in reference, equal to 1.1 volts on the ATmega168 or ATmega328 and 2.56 volts on the ATmega8 (not available on the Arduino Mega)
-    INTERNAL1V1: a built-in 1.1V reference (Arduino Mega only)
-    INTERNAL2V56: a built-in 2.56V reference (Arduino Mega only)
-    EXTERNAL: the voltage applied to the AREF pin (0 to 5V only) is used as the reference. 
-*/
-
 vm_word_t a_in(vm_word_t nr, vm_word_t a_ref)
 {
-	IF_LAZY_INIT ( pin_mode[AIN_MAP(nr)] != PINMODE_ANALOG_IN )
-	{
-		pinMode(nr, INPUT);
-		analogReference(a_ref);
-		pin_mode[AIN_MAP(nr)] = PINMODE_ANALOG_IN;
-	}
+	analogReference(a_ref);
 	return analogRead(nr);
 }
 
-/* ------------------------------------------------------------------------ */
-
-void a_out(vm_word_t nr/*, vm_word_t f*/, vm_word_t dc)
+void a_out(vm_word_t nr, vm_word_t dc)
 {
-	IF_LAZY_INIT ( pin_mode[AOUT_MAP(nr)] != PINMODE_ANALOG_OUT )
-	{
-		pinMode(nr, OUTPUT);
-		pin_mode[AOUT_MAP(nr)] = PINMODE_ANALOG_OUT;
-	}
 	analogWrite(nr, dc);/* 0..255 */
 }
 
-/* ------------------------------------------------------------------------ */
+void set_d_in(vm_word_t nr)
+{
+	pinMode(nr, INPUT);
+}
 
+void set_d_out(vm_word_t nr)
+{
+	pinMode(nr, OUTPUT);
+}
+
+void set_a_in(vm_word_t nr)
+{
+	pinMode(nr, INPUT);
+}
+
+void set_a_out(vm_word_t nr)
+{
+	pinMode(nr, OUTPUT);
+}
 
--- library/arduino/iowrap.h
+++ library/arduino/iowrap.h
@@ -1,15 +1,78 @@
 
+#ifndef __ARDUINO_IOWRAP_H__
+#define __ARDUINO_IOWRAP_H__
+
 #define _VM_EXPORT_ 
 typedef int vm_word_t;
 typedef long vm_dword_t;
 
-_VM_EXPORT_ vm_word_t d_in(vm_word_t nr); /* simple digital input */
-_VM_EXPORT_ void d_out(vm_word_t nr, vm_word_t v); // simple digital output
+/**
+	<A read digital input>
+	@param nr number of pin
+*/
+_VM_EXPORT_ vm_word_t d_in(vm_word_t nr);
+
+/**
+	<A write digital output>
+	@param nr number of pin
+	@param v output value 0..1
+*/
+_VM_EXPORT_ void d_out(vm_word_t nr, vm_word_t v);
+
+
+/**
+	<A read analog input>
+	@param nr number of pin
+	@param a_ref ADC reference voltage
+	    0 DEFAULT: the default analog reference of 5 volts (on 5V Arduino boards) or 3.3 volts (on 3.3V Arduino boards)
+	    1 INTERNAL: an built-in reference, equal to 1.1 volts on the ATmega168 or ATmega328 and 2.56 volts on the ATmega8 (not available on the Arduino Mega)
+	    2 INTERNAL1V1: a built-in 1.1V reference (Arduino Mega only)
+	    3 INTERNAL2V56: a built-in 2.56V reference (Arduino Mega only)
+	    4 EXTERNAL: the voltage applied to the AREF pin (0 to 5V only) is used as the reference. 
+	@return analog reading 0..1023
+*/
 _VM_EXPORT_ vm_word_t a_in(vm_word_t nr, vm_word_t a_ref);
+
+/**
+	<A write analog output>
+	@param nr number of pin
+	@param dc output value from 0 (always off) to 255 (always on)
+*/
 _VM_EXPORT_ void a_out(vm_word_t nr, vm_word_t dc);
 
+/**
+	<A set port as digital input>
+	@param nr number of pin
+*/
+_VM_EXPORT_ void set_d_in(vm_word_t nr);
+
+/**
+	<A set port as digital output>
+	@param nr number of pin
+*/
+_VM_EXPORT_ void set_d_out(vm_word_t nr);
+
+/**
+	<A set port as analog input>
+	@param nr number of pin
+*/
+_VM_EXPORT_ void set_a_in(vm_word_t nr);
+
+/**
+	<A set port as analog output>
+	@param nr number of pin
+*/
+_VM_EXPORT_ void set_a_out(vm_word_t nr);
+
+/**
+	<A return number of milliseconds since start of machine>
+*/
 _VM_EXPORT_ vm_dword_t time_ms();
+
+/**
+	<A return number of microseconds since start of machine>
+*/
 _VM_EXPORT_ vm_dword_t time_us();
 
-_VM_EXPORT_ void dummy(vm_word_t nr, vm_word_t a_ref, vm_word_t *x, vm_word_t * y);
+#endif
 
