0b4b32d733665f4ea1ff0136d0444cf1c73d68b0
vd <v.dorrich@gmail.com>
2012-07-06 21:45:14 +0200

    added note to license

--- library/i2c/myWire.h
+++ library/i2c/myWire.h
@@ -17,6 +17,9 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
   Modified 2012 by Todd Krein (todd@krein.org) to implement repeated starts
+
+  heavily modified by vdorr 2012
+
 */
 
 #ifndef TwoWire_h
--- library/i2c/twi_async.h
+++ library/i2c/twi_async.h
@@ -15,6 +15,9 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  heavily modified by vdorr 2012
+
 */
 
 #ifndef twi_h
