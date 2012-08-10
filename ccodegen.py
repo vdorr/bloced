28ebee13872b84802ab0d6da5f376502d3635049
vd <v.dorrich@gmail.com>
2012-08-10 15:52:00 +0200

    draft markers

--- ccodegen.py
+++ ccodegen.py
@@ -161,8 +161,20 @@ def __get_initdel_value(code, n, state_var_prefix, del_type, tmp_slot, expr) :
 	code.append("}")
 
 
+def __begin_marker(n, code, markers) :
+	markers.append((n, ))
+	code.append("/* begin mark {} */ {{".format(len(markers)))
+
+
+def __end_marker(n, code, markers) :
+	v, = markers.pop()
+	assert(n==v)
+	markers.append((n, len(code), ))
+	code.append("}} /* end mark {} */".format(len(markers)))
+
+
 def __post_visit(g, code, tmp, tmp_args, subtrees, expd_dels, types, known_types,
-		dummies, state_var_prefix, pipe_vars, libs_used, evaluated, n, visited,
+		dummies, state_var_prefix, pipe_vars, libs_used, evaluated, markers, n, visited,
 		nesting=False) :
 #	print "__post_visit:", n.to_string()
 
@@ -223,6 +235,7 @@ def __post_visit(g, code, tmp, tmp_args, subtrees, expd_dels, types, known_types
 		assert(n==del_in)
 #		is_initdel = core.compare_proto_to_type(del_out.prototype, core.InitDelayOutProto)
 		if not del_out in evaluated :# or is_initdel :
+			__begin_marker(n, code, markers)
 #			print here(), del_in.terms
 			del_type = types[del_out, del_out.terms[0], 0]
 			slot = add_tmp_ref(tmp, [ (del_in, del_in.terms[0], 0) ], slot_type=del_type)
@@ -232,6 +245,7 @@ def __post_visit(g, code, tmp, tmp_args, subtrees, expd_dels, types, known_types
 				__get_initdel_value(code, n, state_var_prefix, del_type, slot, args[0][1])
 			else :
 				code.append("{0}_tmp{1} = {2}del{3};".format(del_type, slot, state_var_prefix, n.nr))
+			__end_marker(n, code, markers)
 		expr = "{0}del{1}={2}".format(state_var_prefix, n.nr, args[0][1])
 
 	elif core.compare_proto_to_type(n.prototype, core.DelayOutProto, core.InitDelayOutProto) :
@@ -245,7 +259,9 @@ def __post_visit(g, code, tmp, tmp_args, subtrees, expd_dels, types, known_types
 				assert(len(args)==1 and len(outs)==0)
 				del_type = types[del_out, del_out.terms[0], 0]
 				slot = add_tmp_ref(tmp, [ (del_in, del_in.terms[0], 0) ], slot_type=del_type)
+				__begin_marker(n, code, markers)
 				__get_initdel_value(code, n, state_var_prefix, del_type, slot, args[0][1])
+				__end_marker(n, code, markers)
 				slot_type, slot = pop_tmp_ref(tmp, del_in, del_in.terms[0], 0)
 				expr = "{0}_tmp{1}".format(slot_type, slot)
 			else :
@@ -261,6 +277,7 @@ def __post_visit(g, code, tmp, tmp_args, subtrees, expd_dels, types, known_types
 		assert(len(outputs)==1 and len(outputs[0][2])==1)
 		subtrees[outputs[0][2][0]] = expr
 	else :
+		__begin_marker(n, code, markers)
 		if len(outputs) == 0 :
 			code.append(expr + ";")
 		elif len(outputs) == 1 :
@@ -275,6 +292,7 @@ def __post_visit(g, code, tmp, tmp_args, subtrees, expd_dels, types, known_types
 				code.append("{0}_tmp{1} = {2};".format(expr_slot_type, expr_slot, expr))
 		else :
 			code.append(expr + ";")
+		__end_marker(n, code, markers)
 
 	evaluated[n] = True
 
@@ -294,9 +312,10 @@ def codegen(g, expd_dels, meta, types, known_types, pipe_vars, libs_used, task_n
 	state_var_prefix = task_name + "_"
 	evaluated = {}
 	tmp_args = { type_name : None for type_name in core.KNOWN_TYPES }
+	markers = []
 
 	post_visit_callback = partial(__post_visit, g, code, tmp, tmp_args, subtrees,
-		expd_dels, types, known_types, dummies, state_var_prefix, pipe_vars, libs_used, evaluated)
+		expd_dels, types, known_types, dummies, state_var_prefix, pipe_vars, libs_used, evaluated, markers)
 
 #	pprint(g)
 	dft_alt(g, post_visit=post_visit_callback)
